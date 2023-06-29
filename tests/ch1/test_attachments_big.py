7aa86e4f761122b3f5d29e51cb9bdb36ed2b3361
Egor Ivanov <cheshi.mantu@protonmail.com>
2023-06-29 19:54:48 +0300

    added stdout output and pauses

--- tests/ch1/test_attachments_big.py
+++ tests/ch1/test_attachments_big.py
@@ -9,16 +9,23 @@ from allure import attachment_type
 @allure.feature("sending attachments")
 @allure.title("Sending 8 Mb JPG attachments")
 def test_attach_bigimage():
+    print("After this step we'll have a pause for 10 seconds")
     with allure.step("Sending big 8 megabytes JPG attach"):
         allure.attach.file(os.path.join("resources", "big-image.jpg"), name="8Mb JPG example", attachment_type=attachment_type.JPG)
+    time.sleep(10)
+    print("After this step we'll have a pause for 1 second")
     with allure.step("HTML attach"):
         allure.attach("<h1>Example html attachment</h1>", name="HTML example", attachment_type=attachment_type.HTML)
+    time.sleep(1)
     with allure.step("txt attach"):
         allure.attach("Some text content", name="TXT example", attachment_type=attachment_type.TEXT)
+    time.sleep(5)
     with allure.step("CSV attach"):
         allure.attach.file(os.path.join("resources", "big-table.csv"), name="9,2 Mb CSV example", attachment_type=attachment_type.CSV)
+    time.sleep(10)
     with allure.step("JSON attach"):
         allure.attach(json.dumps({"first": 1, "second": 2}, indent=2),name="JSON example", attachment_type=attachment_type.JSON)
+    time.sleep(10)
     with allure.step("URI list attach"):        
         allure.attach("\n".join(["https://github.com/allure-framework", "https://github.com/allure-examples"]), name="URI List example", attachment_type=attachment_type.URI_LIST)
 
