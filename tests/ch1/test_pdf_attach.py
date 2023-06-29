0c469641e9adbc5430bf38658a82be9c4f8253bd
Egor Ivanov <cheshi.mantu@protonmail.com>
2023-06-29 22:00:26 +0300

    added pauses

--- tests/ch1/test_pdf_attach.py
+++ tests/ch1/test_pdf_attach.py
@@ -12,5 +12,8 @@ def test_pdf_attach():
     print("sending PDF attachment")
     time.sleep(5)
     with allure.step("Sending fillable PDF to report"):
-        print("sending PDF attachment")            
+        print("sending PDF attachment")
+        time.sleep(5)
         allure.attach.file(os.path.join("resources", "fillable.pdf"), name="fillable pdf", attachment_type=attachment_type.PDF)
+        time.sleep(5)
+        print("== just sent it")
