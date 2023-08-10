6eb25dc0a82720791dd720aec6131d4cd4920943
Egor Ivanov <cheshi.mantu@protonmail.com>
2023-08-10 20:28:23 +0300

    added attachments

--- tests/ch1/test_fixtures.py
+++ tests/ch1/test_fixtures.py
@@ -1,11 +1,19 @@
 import allure
 import pytest
+import os
+from allure import attachment_type
 
 @allure.title("Fixture title")
 @pytest.fixture
 def titled_fixture():
     with allure.step("Step one will pass, testing fixture title"):
         pass
+    with allure.step("Sending normal 10 kbytes JPG attach"):
+        allure.attach.file(os.path.join("resources", "small-image.jpg"), name="10 Kb JPG example", attachment_type=attachment_type.JPG)
+    with allure.step("HTML attach"):
+        allure.attach("<h1>Example html attachment</h1>", name="HTML example", attachment_type=attachment_type.HTML)
+    with allure.step("txt attach"):
+        allure.attach.file(os.path.join("resources", "chekhov.txt"), name="letter to the neighbour", attachment_type=attachment_type.TEXT)
     with allure.step("Step two will pass, testing fixture title"):
         pass
     with allure.step("Step three will pass, testing fixture title"):
@@ -19,15 +27,26 @@ def titled_fixture():
 def test_with_fixture_title(titled_fixture):
     with allure.step("Step will just pass, testing fixture title"):
         pass
+    with allure.step("Sending normal 10 kbytes JPG attach"):
+        allure.attach.file(os.path.join("resources", "small-image.jpg"), name="10 Kb JPG example", attachment_type=attachment_type.JPG)
+    with allure.step("HTML attach"):
+        allure.attach("<h1>Example html attachment</h1>", name="HTML example", attachment_type=attachment_type.HTML)
+    with allure.step("txt attach"):
+        allure.attach.file(os.path.join("resources", "chekhov.txt"), name="letter to the neighbour", attachment_type=attachment_type.TEXT)
+
 
 @allure.title("Session level yield fixture")
 @pytest.fixture(scope="session")
 def session_level_yield_fixture():
     with allure.step("Step inside session level fixture"):
         pass
-
+    with allure.step("Sending normal 10 kbytes JPG attach"):
+        allure.attach.file(os.path.join("resources", "small-image.jpg"), name="10 Kb JPG example", attachment_type=attachment_type.JPG)
+    with allure.step("HTML attach"):
+        allure.attach("<h1>Example html attachment</h1>", name="HTML example", attachment_type=attachment_type.HTML)
+    with allure.step("txt attach"):
+        allure.attach.file(os.path.join("resources", "chekhov.txt"), name="letter to the neighbour", attachment_type=attachment_type.TEXT)
     yield
-
     with allure.step("Step inside finalizer session level fixture"):
         pass
 
@@ -38,6 +57,13 @@ def module_level_yield_fixture():
         pass
 
     yield
+
+    with allure.step("Sending normal 10 kbytes JPG attach"):
+        allure.attach.file(os.path.join("resources", "small-image.jpg"), name="10 Kb JPG example", attachment_type=attachment_type.JPG)
+    with allure.step("HTML attach"):
+        allure.attach("<h1>Example html attachment</h1>", name="HTML example", attachment_type=attachment_type.HTML)
+    with allure.step("txt attach"):
+        allure.attach.file(os.path.join("resources", "chekhov.txt"), name="letter to the neighbour", attachment_type=attachment_type.TEXT)
 
     with allure.step("Step inside finalizer module level fixture"):
         pass
@@ -50,6 +76,13 @@ def function_level_yield_fixture():
 
     yield
 
+    with allure.step("Sending normal 10 kbytes JPG attach"):
+        allure.attach.file(os.path.join("resources", "small-image.jpg"), name="10 Kb JPG example", attachment_type=attachment_type.JPG)
+    with allure.step("HTML attach"):
+        allure.attach("<h1>Example html attachment</h1>", name="HTML example", attachment_type=attachment_type.HTML)
+    with allure.step("txt attach"):
+        allure.attach.file(os.path.join("resources", "chekhov.txt"), name="letter to the neighbour", attachment_type=attachment_type.TEXT)
+
     with allure.step("Step inside finalizer function level fixture"):
         pass
 
@@ -58,6 +91,12 @@ def function_level_yield_fixture():
 def session_level_fixture(request):
     with allure.step("Step inside session level fixture"):
         pass
+    with allure.step("Sending normal 10 kbytes JPG attach"):
+        allure.attach.file(os.path.join("resources", "small-image.jpg"), name="10 Kb JPG example", attachment_type=attachment_type.JPG)
+    with allure.step("HTML attach"):
+        allure.attach("<h1>Example html attachment</h1>", name="HTML example", attachment_type=attachment_type.HTML)
+    with allure.step("txt attach"):
+        allure.attach.file(os.path.join("resources", "chekhov.txt"), name="letter to the neighbour", attachment_type=attachment_type.TEXT)
 
     def finalizer():
         with allure.step("Step inside finalizer session level fixture"):
@@ -82,6 +121,12 @@ def module_level_fixture(request):
 def function_level_fixture(request):
     with allure.step("Step inside function level fixture"):
         pass
+    with allure.step("Sending normal 10 kbytes JPG attach"):
+        allure.attach.file(os.path.join("resources", "small-image.jpg"), name="10 Kb JPG example", attachment_type=attachment_type.JPG)
+    with allure.step("HTML attach"):
+        allure.attach("<h1>Example html attachment</h1>", name="HTML example", attachment_type=attachment_type.HTML)
+    with allure.step("txt attach"):
+        allure.attach.file(os.path.join("resources", "chekhov.txt"), name="letter to the neighbour", attachment_type=attachment_type.TEXT)
 
     def finalizer():
         with allure.step("Step inside finalizer function level fixture"):
@@ -93,8 +138,20 @@ def function_level_fixture(request):
 def test_allure_yield_fixture(session_level_yield_fixture, module_level_yield_fixture, function_level_yield_fixture):
     with allure.step("Step inside test_allure_yield_fixture"):
         pass
+    with allure.step("Sending normal 10 kbytes JPG attach"):
+        allure.attach.file(os.path.join("resources", "small-image.jpg"), name="10 Kb JPG example", attachment_type=attachment_type.JPG)
+    with allure.step("HTML attach"):
+        allure.attach("<h1>Example html attachment</h1>", name="HTML example", attachment_type=attachment_type.HTML)
+    with allure.step("txt attach"):
+        allure.attach.file(os.path.join("resources", "chekhov.txt"), name="letter to the neighbour", attachment_type=attachment_type.TEXT)
 
 
 def test_allure_fixture_with_finalizer(session_level_fixture, module_level_fixture, function_level_fixture):
     with allure.step("Step inside test_allure_fixture_with_finalizer"):
         pass
+    with allure.step("Sending normal 10 kbytes JPG attach"):
+        allure.attach.file(os.path.join("resources", "small-image.jpg"), name="10 Kb JPG example", attachment_type=attachment_type.JPG)
+    with allure.step("HTML attach"):
+        allure.attach("<h1>Example html attachment</h1>", name="HTML example", attachment_type=attachment_type.HTML)
+    with allure.step("txt attach"):
+        allure.attach.file(os.path.join("resources", "chekhov.txt"), name="letter to the neighbour", attachment_type=attachment_type.TEXT)
--- tests/ch1/test_mp4_attachments_small.py
+++ tests/ch1/test_mp4_attachments_small.py
@@ -6,6 +6,7 @@ from allure import attachment_type
 @allure.story("smoking pytest")
 @allure.feature("sending attachments")
 @allure.title("Sending mp4 attachment")
+@allure.label("msrv", "Billin")
 def test_attach_smallimage():
     with allure.step("Sending small mp4 attach"):
         allure.attach.file(os.path.join("./resources", "mp4.mp4"), name="mp4file", attachment_type=attachment_type.MP4)
