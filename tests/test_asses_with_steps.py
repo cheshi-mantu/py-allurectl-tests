e3dc5eff672486b2005ce25950a08108b9bddc1b
Egor I <egor.ivanov@pm.me>
2025-02-18 16:32:35 +0100

    adds assert

--- tests/test_asses_with_steps.py
+++ tests/test_asses_with_steps.py
@@ -1,16 +1,11 @@
 import allure
-from allure import attachment_type
-
-import time
-import os
 
 @allure.suite("cloud instance")
 @allure.feature("sending attachments")
 @allure.story("sending JSON attachments")
 @allure.title("Sending JSON attachments")
 def test_json_attach():
-    print("sending JSON attachment")
-    time.sleep(5)
-    with allure.step("Sending fillable PDF to report as JSON request"):
-        print("sending JSON attachment")
-        allure.attach.file(os.path.join("resources", "request_example.json"), name="request example", attachment_type=attachment_type.JSON)
+    with allure.step("define stuff"):
+        pass
+    with allure.step("expected result 2 > 3"):
+        assert(2 > 3)
