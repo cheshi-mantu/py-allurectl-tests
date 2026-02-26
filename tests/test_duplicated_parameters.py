ba04d13e038897c9c2651c896f36b823250ce9fc
Egor I <egor.ivanov@pm.me>
2026-02-26 17:29:24 +0200

    upd

--- tests/test_duplicated_parameters.py
+++ tests/test_duplicated_parameters.py
@@ -1,20 +1,10 @@
 import allure
 
-@allure.feature("Answering questions")
-@allure.story("Answering questionable questions")
-@allure.title("Do stuff and then assert dammit")
-def test_json_attach():
-    with allure.step("define stuff"):
-        pass
-    with allure.step("define stuff"):
-        pass
-    with allure.step("do stuff"):
-        pass
-    with allure.step("do more stuff"):
-        pass
-    with allure.step("do even more stuff"):
-        pass
-    with allure.step("expected result 2 > 3"):
-        pass
-        with allure.step("asserting 2 > 3"):
-            assert(2 > 3)
+@allure.epic("Uploader")
+@allure.feature("Reliability")
+@allure.story("Duplicated parameter")
+@allure.title("Uploader should correctly process duplicated keys in test results")
+def test_should_not_fail_on_duplicated_params():
+    allure.dynamic.parameter("uno", "uno-uno", excluded=None, mode=None)
+    allure.dynamic.parameter("dos", "dos-dos", excluded=None, mode=None)
+    allure.dynamic.parameter("uno", "uno-dos", excluded=None, mode=None)
